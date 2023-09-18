a4743c5df039d36063d001b2d7789a82b83cb4d6
Biancayume13 <144021128+Biancayume13@users.noreply.github.com>
2023-09-18 09:30:20 -0300

    encrementar HTMl, CSS e Index

--- JS/main.js
+++ JS/main.js
@@ -1,2 +1,22 @@
-fetch("https://cataas.com/#/")
-.then()
+const btnmudar = document.getElementById("mudar")
+const imag = document.createElement("imag")
+btnmudar.addEventListener("click", (event) => {
+  gerarGato();
+})
+
+const content = document.getElementById("content");
+gerarGato();
+
+function gerarGato(){
+    fetch("https://cataas.com/cat")
+    .then(res => res.blob())
+    .then(res =>{
+    const url = URL.createObjectURL(res)
+    const img = document.createElement("img");
+    img.src = url
+    content.appendChild(img)
+})
+    .catch(err => {
+    console.log(err)
+})
+}
